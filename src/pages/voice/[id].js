3b699ac22bd6275b24b4a43160fe01d97e3fb725
fariborz0015 <fariborz0015@gmail.com>
2023-05-06 22:45:05 +0430

    fix voice bug

--- src/pages/voice/[id].js
+++ src/pages/voice/[id].js
@@ -3,18 +3,7 @@ import React, { useEffect, useRef } from 'react'
 
 const Voice = (props) => {
   const { query } = useRouter()
-  const audioRef = useRef(null)
-  useEffect(() => {
-    if (navigator.getAutoplayPolicy('mediaelement') === 'allowed') {
-      if (audioRef) {
-        audioRef.current.play()
-      }
-    }else{
-      if (audioRef) {
-        audioRef.current.play()
-      }
-    }
-  }, [audioRef])
+ 
   return (
     <div className="bg-white h-screen max-w-lg w-full mx-auto flex flex-col ">
       <img
@@ -25,7 +14,7 @@ const Voice = (props) => {
       <div className="flex-1 flex justify-center items-center">
         <audio
           autoPlay
-          ref={audioRef}
+     
           src={`/assets/voices/${query.id}.mp3`}
           controls
         ></audio>
