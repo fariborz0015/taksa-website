cd39297af0e986e872bf75789633dc96fe6f192b
fariborz0015 <fariborz0015@gmail.com>
2023-05-23 05:02:28 +0330

    feat: add animation to tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,11 +60,28 @@ module.exports = {
 
       animation: {
         shimmer: 'custom-shimmer 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'soft-bunce': 'moVebounce 5s linear infinite',
+        'fast-puls': 'fast-puls 1s linear 1s infinite',
       },
       keyframes: {
         'custom-shimmer': {
           '0%, 100%': { opacity: '1' },
           '50%': { opacity: '0.5' },
+        },
+        'fast-puls': {
+          '0%': {
+            opacity: 0,
+          },
+          '75%': {
+            opacity: 1,
+          },
+          to: {
+            opacity: 0,
+          },
+        },
+        moVebounce: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(50px)' },
         },
       },
     },
